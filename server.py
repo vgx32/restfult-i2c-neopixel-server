c4d5bc8962897233cc1289a2fbcc691e1519c4fe
sa <salekhnovich@gmail.com>
2016-01-23 14:53:30 -0800

    added code to log serial communication from arduino

--- server.py
+++ server.py
@@ -1,11 +1,35 @@
 import time
 import serial
+import sys
+import threading
+
 
 def connect(device):
     try:
-        return serial.Serial(device, baudrate=9600, timeout=0)
+        return serial.Serial(device, baudrate=9600)
     except:
         print("failed to connect to", device)
+
+# assume data is a byte array
+def writeData(con, data):
+  print "tx:" + data.tostring()
+  con.write(data)
+
+def runDebugLog(con):
+  debugThread = threading.Thread(target=debugPrints, args=(con,))
+  debugThread.daemon = True
+  debugThread.start()
+  return debugThread
+
+def debugPrints(con):
+  while True:
+    readData(con)
+
+def readData(con):
+  # con.timeout
+  data = con.readline()
+  sys.stdout.write("RX(%s):%s" %(len(data), data))
+  return data
 
 
 device = "/dev/ttyACM0"
@@ -21,6 +45,8 @@ testData = ("\x20\x00\x00" +
             "\x00\x20\x00" +
             "\x00\x00\x20" +
             "\x20\x20\x00")
+
+
 
 def packagePixelData(displayMode, pixelValues):
     if displayMode in modes:
