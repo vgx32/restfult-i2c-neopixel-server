d47a3cfbd635eccbc73da8a5bdfa0cb0fe231b19
sa <salekhnovich@gmail.com>
2016-01-23 17:56:15 -0800

    added test stubs for flask client and unittest for mode conversion function

--- test.py
+++ test.py
@@ -12,6 +12,27 @@ class NeoPixelServerTests(unittest.TestCase):
         # }
         # self.assertEqual(1,2)
 
+    def test_mode_conversion(self):
+        modes = [
+            ("cycle",  "\x63"),
+            ("both",  "\x62"),
+            ("all-on",  "\x61")
+        ]
+        for inVal, expectedResult in modes:
+            self.assertEqual(
+                arduino_interface.modeToModeChar(inVal),
+                expectedResult)
+        
+
+    def test_api_post_pixel_data(self):
+        pass
+
+    def test_api_html_endpoint(self):
+        pass
+
+    def test_api_get_current_display_data(self):
+        pass
+
 
 if __name__ == '__main__':
     unittest.main(failfast=True)
